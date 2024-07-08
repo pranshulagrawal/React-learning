c88f82dbc8dfd360805c51601805abcea859cc65
1192789 <pranshulagrawal9269@gmail.com>
2024-07-08 17:32:03 +0530

    rendering object using map

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,11 +1,40 @@
 import React from "react";
 
 const Fruits = () => {
-  const fruits = ["Apple", "Mango", "Banana", "Orange", "Papaya"];
+  //   const fruits = ["Apple", "Mango", "Banana", "Orange", "Papaya"];
+  const fruits = [
+    {
+      name: "Apple",
+      price: 20,
+      emoji: "🍎",
+    },
+    {
+      name: "Banana",
+      price: 40,
+      emoji: "🍌",
+    },
+    {
+      name: "Orange",
+      price: 50,
+      emoji: "🍊",
+    },
+    {
+      name: "Mango",
+      price: 70,
+      emoji: "🥭",
+    },
+    {
+      name: "Grapes",
+      price: 100,
+      emoji: "🍇",
+    },
+  ];
   return (
     <div>
       {fruits.map((fruit) => (
-        <li key={fruit}>{fruit}</li>
+        <li key={fruit}>
+          {fruit.emoji} {fruit.name} {fruit.price}
+        </li>
       ))}
     </div>
   );
