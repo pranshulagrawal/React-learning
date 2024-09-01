a17781929b6771644e1da52a1fac574688f74aad
Pranshulagrawal <Pranshulagrawal9269@gmail.com>
2024-09-01 23:55:27 +0530

    node tree modified version with minor changes

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./styles.css";
 import Table from "../../components/Table";
 import SearchHeader from "../../components/search-header";
+import DataTree from "../../components/search-header/tree";
 
 const HomeScreen: React.FC = () => {
   return (
     <div>
-      <SearchHeader />
+      <DataTree />
       <Table />
     </div>
   );
