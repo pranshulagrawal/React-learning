e1d6c8dd8b4cf5be157cff4a08535c060e9bf505
Pranshulagrawal <Pranshulagrawal9269@gmail.com>
2024-09-07 22:18:40 +0530

    added api endpoints

--- src/services/Dataservice.tsx
+++ src/services/Dataservice.tsx
@@ -3,9 +3,22 @@ import { OrgNode } from "../model/node-model";
 
 export class DataService {
   async fetchNodes(): Promise<OrgNode[]> {
-    const response = await fetch("nested_organizational_data_india.json");
-    const data = await response.json();
-    return data.nodes;
+    try {
+      // Replace the URL below with your actual API endpoint
+      const response = await fetch(
+        "https://https://ritzybackend-60a9b931e77d.herokuapp.com/api/org-nodes/hierarchy"
+      );
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      return data.nodes; // Adjust based on the structure of your API response
+    } catch (error) {
+      console.error("Error fetching nodes:", error);
+      throw error; // Re-throw the error so it can be handled by the caller
+    }
   }
 
   async fetchNodesDetail(): Promise<OrgNodeDetails[]> {
